be2ffce735f1210a6b4d17f2133d4ecec682ede6
easerbetty <easerbetty@gmail.com>
2025-03-26 15:27:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const fungusData = [
     { name: "Blumeria graminis", image: "https://inaturalist-open-data.s3.amazonaws.com/photos/237099909/medium.jpeg" },
     { name: "Erysiphe necator", image: "https://inaturalist-open-data.s3.amazonaws.com/photos/159236695/medium.jpg" },
     { name: "Neoerysiphe galeopsidis", image: "https://inaturalist-open-data.s3.amazonaws.com/photos/250270034/medium.jpeg" },
-    { name: "Podosphaera leucotricha", image: "https://inaturalist-open-data.s3.amazonaws.com/photos/280981335/large.jpeg" },
 ];
 
 let currentIndex = 0;
@@ -17,9 +16,9 @@ function nextCard() {
     // Обновление изображения и названия гриба
     document.getElementById("fungus-image").src = fungusData[currentIndex].image;
     document.getElementById("fungus-name").innerText = fungusData[currentIndex].name;
-    
-    // Убираем текст с ответом
-    document.getElementById("answer").innerText = ""; // Скрываем ответ после переключения карточки
+
+    // Скрываем ответ после перехода на следующую карточку
+    document.getElementById("answer").innerText = ""; 
 }
 
 function showAnswer() {
